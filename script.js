3e74649743e8038eba736da30a93ebeddfee64f5
zeludev <142579490+zeludev@users.noreply.github.com>
2023-08-22 11:42:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 let menuVisible = false;
 
 //Función que oculta o muesra el menú
-function mostrarOcultar(){
-    if(menuVisible){
-        document.getElementById("nav").classList = "";
-        menuVisible = false;
-    }
-    else{
-        document.getElementById("nav").classList = "responsive"
-        menuVisible = true;
-    }
-}
-
-function seleccionar(){
-    //hay que ocultar el menu una vez que selecciono una opcion
+function mostrarOcultar() {
+  if (menuVisible) {
     document.getElementById("nav").classList = "";
     menuVisible = false;
+  } else {
+    document.getElementById("nav").classList = "responsive";
+    menuVisible = true;
+  }
 }
 
+function seleccionar() {
+  //hay que ocultar el menu una vez que selecciono una opcion
+  document.getElementById("nav").classList = "";
+  menuVisible = false;
+}
